f79e03cf9c7fb0790fc1a0b0ddaef6c651201e7c
Aussoonne <134248133+Aussoonne@users.noreply.github.com>
2025-03-10 19:56:26 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "Lune Bot",
+  botOwner: '(Σ a + 3 ) = ?', // Your name
   ownerNumber: '917023951514', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Este es un bot.",
   version: "1.0.0",
 };
 
